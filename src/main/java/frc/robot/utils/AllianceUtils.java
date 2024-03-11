e918edd71ea3341f263f79370ccc8c026204b201
furki <furkan.turkan1@bahcesehir.edu.tr>
2024-03-11 10:35:01 +0300

    Remove unused util function

--- src/main/java/frc/robot/utils/AllianceUtils.java
+++ src/main/java/frc/robot/utils/AllianceUtils.java
@@ -13,33 +13,8 @@ public class AllianceUtils {
 
     static Optional<Alliance> ally = DriverStation.getAlliance();
 
-    public static Pose2d allianceToField(Pose2d alliancePose) {
-        if (ally.isPresent()) {
-            switch (ally.get()) {
-                case Blue:
-                    return alliancePose;
-                case Red:
-                    return new Pose2d(
-                            new Translation2d(
-                                    FieldConstants.fieldLength - alliancePose.getX(),
-                                    alliancePose.getY()
-                            ),
-                            alliancePose.getRotation().minus(Rotation2d.fromRadians(Math.PI))
-                    );
-                default:
-                    return null;
-            }
-        } else {
-            return null;
-        }
-
-    }
-
     public static boolean isBlue() {
-        //Red seçince blue planı çalışıyo
-        //Blue seçince red planı çalışıyo
         if (ally.isPresent()) {
-
             return ally.get().equals(Alliance.Red);
         } else {
             return true;
